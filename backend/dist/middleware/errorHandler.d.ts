77c59286520ed3f58bf38543d2f608e72bf8495b
VikashBhandari33 <vikashbhandari392@gmail.com>
2026-03-14 16:18:28 +0530

    chore: untrack generated build files and dependencies

--- backend/dist/middleware/errorHandler.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare class AppError extends Error {
-    statusCode: number;
-    error: string;
-    constructor(statusCode: number, error: string, message: string);
-}
-export declare function asyncHandler(fn: (req: Request, res: Response, next: NextFunction) => Promise<unknown>): (req: Request, res: Response, next: NextFunction) => void;
-export declare function errorHandler(err: Error, _req: Request, res: Response, _next: NextFunction): void;
-//# sourceMappingURL=errorHandler.d.ts.map
--- node_modules/cecb-backend/tmp/test-id1.ts
+++
@@ -1,3 +0,0 @@
-import { sha3_256 } from 'js-sha3';
-const ph = "3bc2eec077d853b0e1eaccad4a9d70cc2818c3d3147...".slice(0, 64); // I don't have the full hex here but I can guess
-// Actually I'll just use a script that has the full hex from DB
